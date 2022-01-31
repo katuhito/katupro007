b1cf0be82c1cab7fbfba4bb13c70038209ec1ff8
Katuhito Hara <rahakatuhito@gmail.com>
2022-01-31 11:53:51 +0900

    settings

--- katupro007/settings.py
+++ katupro007/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'katuapp'
+    'katuapp',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
